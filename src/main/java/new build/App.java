ba2d32750d81604bf87db71bc021817bcb88cf9c
syamprasadsiddabattula <syamprasadsiddbattula@gmail.com>
2023-01-21 23:46:28 +0530

    syam new maven project

---
+++ src/main/java/new build/App.java
@@ -0,0 +1,13 @@
+package new build;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
